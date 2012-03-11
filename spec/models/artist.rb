77c484fd8dc6edefb6d403564c1603b2b8edc37c
David J. Brenes <davidjbrenes@gmail.com>
2012-03-12 00:12:27 +0100

    Added VCR and included in the testing

--- spec/models/artist.rb
+++ spec/models/artist.rb
@@ -1,16 +1,34 @@
 require 'lib/lastfm_path_finder'
+
+require 'vcr'
+
+VCR.configure do |c|
+  c.allow_http_connections_when_no_cassette = true
+  c.cassette_library_dir = 'spec/vcr'
+  c.hook_into :webmock # or :fakeweb
+end
+
 describe LastfmPathFinder::Artist do 
-  
+
   it "gets data for existing artists from Last.FM" do
-    artist = LastfmPathFinder::Artist.find_in_lastfm "pink floyd"
-    artist.should_not be_nil
-    artist.id.should be_eql("pink-floyd")
-    artist.name.should == "Pink Floyd"
+
+    VCR.use_cassette('lastfm', :record => :new_episodes) do
+      artist = LastfmPathFinder::Artist.find_in_lastfm "pink floyd"
+      artist.should_not be_nil
+      artist.id.should be_eql("pink-floyd")
+      artist.name.should == "Pink Floyd"
+    end
+
   end  
 
   it "handles non-existing artists when retrieving info from Last.FM" do
-    artist = LastfmPathFinder::Artist.find_in_lastfm "who is pink?"
-    artist.should be_nil
+    
+    VCR.use_cassette('lastfm', :record => :new_episodes) do
+      artist = LastfmPathFinder::Artist.find_in_lastfm "who is pink?"
+      artist.should be_nil
+    end
+    
   end
+
 
 end
