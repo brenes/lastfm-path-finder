812ccaca9fa065d2c48e0d7ef8ec45201cb91700
David J. Brenes <davidjbrenes@gmail.com>
2012-07-27 20:24:57 +0200

    When requesting related artists, if artists are not found LastFm return an error

--- spec/models/path.rb
+++ spec/models/path.rb
@@ -2,22 +2,7 @@ require 'lib/lastfm_path_finder'
 
 describe LastfmPathFinder::Path do
 
-  describe "when there's no path" do
-
-    let(:from) {LastfmPathFinder::Artist.new(:name => "from")}
-    let(:to) {LastfmPathFinder::Artist.new(:name => "to")}
-    let(:path) do
-      VCR.use_cassette('lastfm', :record => :new_episodes) do
-        LastfmPathFinder::Finder.find from, to
-      end
-    end
-
-    subject { path }
-    it { should_not be_found }
-
-  end
-
-  context "when there's path" do
+  context "when there's a direct path between artists" do
 
     let(:from) {LastfmPathFinder::Artist.new(:name => "Pink Floyd")}
     let(:to) {LastfmPathFinder::Artist.new(:name => "David Gilmour")}
